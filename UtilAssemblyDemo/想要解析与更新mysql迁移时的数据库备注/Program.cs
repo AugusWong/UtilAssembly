dfdb1ad1ac1ad5fb6d2d7cbd6d9816ad67db851d
will <will>
2020-07-10 15:09:43 +0800

    提交测试迁移更新mysql数据库备注代码

---
+++ UtilAssemblyDemo/想要解析与更新mysql迁移时的数据库备注/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace 想要解析与更新mysql迁移时的数据库备注
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// 更新表相关注释
+        /// </summary>
+        /// <param name="tableName"></param>
+        public void ReparirDBComment(string tableName)
+        {
+            var sql_column = $@"SELECT
+                            table_name,
+	                        COLUMN_Name,
+	                        COLUMN_COMMENT
+                        FROM
+                            information_schema.COLUMNS
+                        WHERE
+                            TABLE_SCHEMA = 'orderservicedb' ";
+
+            var sql_table = $@"SELECT TABLE_NAME,
+                                TABLE_COMMENT
+                                FROM information_schema.TABLES
+                                WHERE TABLE_SCHEMA='orderservicedb' AND TABLE_TYPE='base table' ";
+            if (!tableName.IsNullOrWhiteSpace())
+            {
+                sql_column += $" and table_name='{tableName}' ";
+                sql_table += $" and table_name='{tableName}' ";
+            }
+            var columnList = _dapperRepository.Query<Table_Column>(sql_column).ToList();
+            var tableList = _dapperRepository.Query<Table>(sql_table).ToList();
+            if (tableList.Any() && columnList.Any())
+            {
+                var dllName = "LinkedX.OrderService.Core";
+                var path = new FileInfo(Assembly.GetEntryAssembly().Location).DirectoryName;
+
+                var assembly = Assembly.Load(dllName);
+                var types = assembly?.GetTypes();
+                var entityBaseList = types?
+                    .Where(t => t.IsClass
+                    && !t.IsGenericType
+                    && !t.IsAbstract
+                    && (typeof(EntityBase<Guid>).IsAssignableFrom(t) || typeof(EntityBase<int>).IsAssignableFrom(t))
+                    ).ToList();
+
+                var entityList = types?
+                    .Where(t => t.IsClass
+                    && !t.IsGenericType
+                    && !t.IsAbstract
+                    && (typeof(Entity).IsAssignableFrom(t) || typeof(Entity<Guid>).IsAssignableFrom(t) || typeof(Entity<int>).IsAssignableFrom(t))
+                    ).ToList();
+
+                var xmlPath = Path.Combine(path, dllName + ".xml");
+                FileStream fs = new FileStream(xmlPath, FileMode.Open, FileAccess.ReadWrite);
+                var xmlObj = new QueryCoreXml().Deserialize(fs);
+
+                // var member = xmlObj.Members.MemberList.Where(w => w.Name.ToLower().Contains("transactionbill"));
+
+                var group = columnList.GroupBy(g => g.Table_Name);
+                var descriptionList = new List<DbDescription>();
+                foreach (var item in group)
+                {
+                    var add = new DbDescription
+                    {
+                        Name = item.Key,
+                        Description = tableList.Where(w => w.Table_Name == item.Key).Select(s => s.Table_Comment).FirstOrDefault(),
+                        Column = new List<DbDescription>()
+                    };
+                    foreach (var column in item)
+                    {
+                        add.Column.Add(new DbDescription
+                        {
+                            Name = column.Column_Name,
+                            Description = column.Column_Comment
+                        });
+                    }
+                    descriptionList.Add(add);
+                }
+
+                var xmlDescriptionList = new List<DbDescription>();
+                var wantedClassList = new List<string>();
+                foreach (var member in xmlObj.Members.MemberList)
+                {
+                    var className = "";
+                    if (member.Name.StartsWith("T"))
+                    {
+                        var index = member.Name.LastIndexOf('.');
+                        className = member.Name.Substring(index + 1).ToLower();
+                        var dbtable = descriptionList.FirstOrDefault(f => f.Name.ToLower().Contains(className));
+                        if (dbtable != null)
+                        {
+                            dbtable.Description = member.Summary;
+                            var colonIndex = member.Name.IndexOf(":");
+                        }
+
+                    }
+                }
+
+                //entityList.ForEach
+            }
+        }
+    }
+}
---
+++ UtilAssemblyDemo/想要解析与更新mysql迁移时的数据库备注/ReparirDBCommentDto.cs
@@ -0,0 +1,83 @@
+﻿using Abp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace LinkedX.OrderService.Repairs.Dto
+{
+    class ReparirDBCommentDto
+    {
+    }
+
+    public class Table_Column
+    {
+        /// <summary>
+        /// 表名
+        /// </summary>
+        public string Table_Name { get; set; }
+        /// <summary>
+        /// 列名
+        /// </summary>
+        public string Column_Name { get; set; }
+        /// <summary>
+        /// 列注释
+        /// </summary>
+        public string Column_Comment { get; set; }
+    }
+
+    public class Table
+    {
+        /// <summary>
+        /// 表名
+        /// </summary>
+        public string Table_Name { get; set; }
+        /// <summary>
+        /// 表注释
+        /// </summary>
+        public string Table_Comment { get; set; }
+    }
+    [XmlRoot("doc")]
+    public class QueryCoreXml
+    {
+        [XmlElement("assembly")]
+        public QueryAssemblyXml Assembly { get; set; }
+        [XmlElement("members")]
+        public QueryMembersXml Members { get; set; }
+    }
+    public class QueryAssemblyXml
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+    }
+    public class QueryMembersXml
+    {
+        [XmlElement("member")]
+        public List<MemberXml> MemberList { get; set; }
+    }
+    public class MemberXml
+    {
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+        [XmlElement("summary")]
+        public string Summary { get; set; }
+        [XmlElement("param")]
+        public List<QueryParaXml> ParamList { get; set; }
+        [XmlElement("returns")]
+        public String Return { get; set; }
+    }
+
+    public class QueryParaXml
+    {
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+    }
+
+    public class DbDescription
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<DbDescription> Column { get; set; }
+    }
+
+}
